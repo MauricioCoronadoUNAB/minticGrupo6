d266ff518d39d056c299fd6674950f5d81c5c399
Mauricio Coronado <mauricio.coronado08@gmail.com>
2022-12-21 17:11:16 -0500

    refactoring_adapter: divide into functions adapter

--- app/src/main/java/com/example/projectg104/Adapters/ProductAdapter.java
+++ app/src/main/java/com/example/projectg104/Adapters/ProductAdapter.java
@@ -12,8 +12,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.app.AlertDialog;
 
+import androidx.annotation.NonNull;
+
 import com.example.projectg104.DB.DBFirebase;
 import com.example.projectg104.Entities.Product;
+import com.example.projectg104.ProductUtil;
+import com.example.projectg104.Util;
 import com.example.projectg104.ViewUtil;
 import com.example.projectg104.view.ProductListActivity;
 import com.example.projectg104.view.ProductDetailsActivity;
@@ -21,10 +25,20 @@ import com.example.projectg104.view.ProductEditActivity;
 import com.example.projectg104.R;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class ProductAdapter extends BaseAdapter {
     private Context context;
     private ArrayList<Product> arrayProducts;
+
+    //---
+    private ImageView imgProduct;
+    private TextView textNameProduct;
+    private TextView textDescriptionProduct;
+    private TextView textPriceProduct;
+    private Button btnEditTemplate;
+    private Button btnDeleteTemplate;
+    //---
 
     public ProductAdapter(Context context, ArrayList<Product> arrayProducts) {
         this.context = context;
@@ -46,47 +60,54 @@ public class ProductAdapter extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
         LayoutInflater layoutInflater = LayoutInflater.from(this.context);
         view = layoutInflater.inflate(R.layout.product_list_item_layout, null);
-
-        ImageView imgProduct = (ImageView) view.findViewById(R.id.imgProduct);
-        TextView textNameProduct = (TextView) view.findViewById(R.id.textNameProduct);
-        TextView textDescriptionProduct = (TextView) view.findViewById(R.id.textDescriptionProduct);
-        TextView textPriceProduct = (TextView) view.findViewById(R.id.textPriceProduct);
-        Button btnEditTemplate = (Button) view.findViewById(R.id.btnEditTemplate);
-        Button btnDeleteTemplate = (Button) view.findViewById(R.id.btnDeleteTemplate);
-
+        bindFields(view);
         Product product = arrayProducts.get(i);
-
+        setFields(product);
+        initEventButton(product);
+        return view;
+    }
+    //---
+    private void bindFields(@NonNull View view){
+        imgProduct = (ImageView) view.findViewById(R.id.imgProduct);
+        textNameProduct = (TextView) view.findViewById(R.id.textNameProduct);
+        textDescriptionProduct = (TextView) view.findViewById(R.id.textDescriptionProduct);
+        textPriceProduct = (TextView) view.findViewById(R.id.textPriceProduct);
+        btnEditTemplate = (Button) view.findViewById(R.id.btnEditTemplate);
+        btnDeleteTemplate = (Button) view.findViewById(R.id.btnDeleteTemplate);
+    }
+    private void setFields(@NonNull Product data){
         //byte[] image = product.getImage();
         //Bitmap bitmap  = BitmapFactory.decodeByteArray(image, 0, image.length );
         //imgProduct.setImageBitmap(bitmap);
-
-        textNameProduct.setText(product.getName());
-        textDescriptionProduct.setText(product.getDescription());
-        int Col = product.getPrice() * 5000;
-        int Usd = product.getPrice();
+        int Col = data.getPrice() * 5000;
+        int Usd = data.getPrice();
         String prices = "USD: "+Usd;
+        textNameProduct.setText(data.getName());
+        textDescriptionProduct.setText(data.getDescription());
         textPriceProduct.setText(prices);
-
-        ViewUtil.insertUriToImageView(context,imgProduct,product.getImage());
-
+        ViewUtil.insertUriToImageView(context,imgProduct,data.getImage());
+    }
+    private void initEventButton(Product product){
         imgProduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(context.getApplicationContext(), ProductDetailsActivity.class);
-                intent.putExtra("id", String.valueOf(product.getId()));
-                intent.putExtra("name", String.valueOf(product.getName()));
-                intent.putExtra("description", String.valueOf(product.getDescription()));
-                intent.putExtra("price", String.valueOf(product.getPrice()));
-                intent.putExtra("image", String.valueOf(product.getImage()));
+                Map<String,Object> productMap = ProductUtil.toProductMap(product);
+                for (Map.Entry entry: productMap.entrySet()) {
+                    intent.putExtra(entry.getKey().toString(), Util.getString(entry.getValue(),""));
+                }
                 context.startActivity(intent);
             }
         });
-
         btnEditTemplate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(context.getApplicationContext(), ProductEditActivity.class);
+//                Map<String,Object> productMap = ProductUtil.toProductMap(product);
                 intent.putExtra("edit", true);
+//                for (Map.Entry entry: productMap.entrySet()) {
+//                    intent.putExtra(entry.getKey().toString(), Util.getString(entry.getValue(),""));
+//                }
                 intent.putExtra("id", product.getId());
                 intent.putExtra("name", product.getName());
                 intent.putExtra("description", product.getDescription());
@@ -98,7 +119,6 @@ public class ProductAdapter extends BaseAdapter {
                 context.startActivity(intent);
             }
         });
-
         btnDeleteTemplate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -108,24 +128,23 @@ public class ProductAdapter extends BaseAdapter {
                         .setPositiveButton("SI", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
-                                DBFirebase dbFirebase = new DBFirebase();
-                                dbFirebase.deleteData(product.getId());
+                                deleteProduct(product.getId());
                                 Intent intent = new Intent(context.getApplicationContext(), ProductListActivity.class);
                                 context.startActivity(intent);
                             }
                         })
                         .setNegativeButton("NO", new DialogInterface.OnClickListener() {
                             @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-
-                            }
+                            public void onClick(DialogInterface dialogInterface, int i) {}
                         });
                 AlertDialog dialog = builder.create();
                 dialog.show();
             }
 
-        });
-
-        return view;
+        });        
+    }
+    private void deleteProduct(String id){
+        DBFirebase dbFirebase = new DBFirebase();
+        dbFirebase.deleteData(id);
     }
 }
